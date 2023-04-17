43bee85e32787b2d2c39f6da8e91ffeeb3cc16f5
0x5c0f <mail@0x5c0f.cc>
2023-04-17 12:18:15 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+## About | 关于
+项目参考来源: 
+> https://github.com/EsunR/Blog-Index
+
+## How to use | 使用方法
+
+安装依赖：
+
+```shell
+yarn install
+```
+
+开发模式：
+
+```shell
+yarn serve
+```
+
+### 编译
+确保已安装 `yarn` 以及安装好了项目依赖之后运行：
+
+```
+$ yarn build
+```
+
+生成的文件将存放在 `/dist` 目录下
+
+### 克隆
+```
+$ git clone --recursive  git@github.com:0x5c0f/index-ss.git
+```
---
+++ src/config.js
@@ -0,0 +1,65 @@
+
+const BLOG_NAME = "一个曾经的小码农...";
+
+const BLOG_URL = "https://tools.0x5c0f.cc";
+
+const GITHUB = "https://github.com/0x5c0f";
+
+const BACKGROUND_IMG_URL = "https://tools.0x5c0f.cc/img/bg-default.png";
+
+const RECORD_NUMBER = "渝ICP备2020011834号-2";
+
+const SORT_DATA = [
+  { sortId: 1, title: "我的主页" },
+  { sortId: 2, title: "博客" },
+  { sortId: 3 , title: "AI工具" },
+];
+
+
+const PAGES_DATA = [
+  {
+    pageId: 1,
+    sortId: 1,
+    title: "主页",
+    subtitle: "一个曾经的小码农...",
+    url: "https://tools.0x5c0f.cc",
+    icon: "https://tools.0x5c0f.cc/img/logo_large.png",
+  },
+  {
+    pageId: 2,
+    sortId: 2,
+    title: "博客",
+    subtitle: "一个曾经的小码农...",
+    url: "https://blog.0x5c0f.cc",
+    icon: "https://blog.0x5c0f.cc/icons/logo_title.png",
+  },
+  {
+    pageId: 3,
+    sortId: 3,
+    title: "ChatGPT-Web",
+    subtitle: "一个曾经的小码农...",
+    url: "https://vchat.51ac.cc",
+    icon: "https://blog.0x5c0f.cc/icons/logo_title.png",
+  },
+];
+
+const SLOGAN = [
+  "欢迎来访",
+  "歡迎来訪",
+  "訪問へようこそ",
+  "嗨，别来无恙",
+  "流水，落花，萤火",
+  "紫雾未起，花开已落",
+];
+
+export default {
+  BLOG_URL,
+  BLOG_NAME,
+  SORT_DATA,
+  PAGES_DATA,
+  SLOGAN,
+  GITHUB,
+  BACKGROUND_IMG_URL,
+  RECORD_NUMBER
+};
+
